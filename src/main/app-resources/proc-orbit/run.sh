0d2cce1c90e78d83145ed650747c213ea4075ef8
joaa <joaa@pc-joaa.corp-deimos.pt>
2014-11-05 15:37:01 +0000

    Add folders proc-coreg, proc-orbit and shared.

---
+++ src/main/app-resources/proc-orbit/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# source the ciop functions (e.g. ciop-log)
+source ${ciop_job_include}
+
+# define the exit codes
+SUCCESS=0
+ERR_GPT=10
+
+# add a trap to exit gracefully
+function cleanExit ()
+{
+   local retval=$?
+   local msg=""
+   case "$retval" in
+     $SUCCESS) msg="Processing successfully concluded";;
+     $ERR_GPT) msg="gpt returned an error";;
+     *) msg="Unknown error";;
+   esac
+   [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
+   exit $retval
+}
+
+trap cleanExit EXIT
+
+# Apply-Orbit-File parameters
+orbitType="`ciop-getparam orbitType`"
+
+mkdir -p $TMPDIR/output
+
+# loop through the inputs
+while read input
+do
+  mkdir -p $TMPDIR/output
+
+  local_input=`echo $input | ciop-copy -o $TMPDIR -`
+  base_input=`basename $local_input | sed "s/.N1//"`
+
+  ciop-log "INFO" "Processing $base_input" 
+
+  /application/shared/bin/gpt.sh -e Apply-Orbit-File \
+    -PorbitType="$orbitType" \
+    -t $TMPDIR/output/${base_input}.dim \
+    -Ssource=$local_input
+  
+  [ $? != 0 ] && exit $ERR_GPT 
+ 
+  tar -C $TMPDIR/output -czf $TMPDIR/output/${base_input}.tgz ${base_input}.dim ${base_input}.data
+  
+  ciop-publish $TMPDIR/output/${base_input}.tgz
+  
+  rm -f $local_input
+  rm -fr $TMPDIR/output
+done
